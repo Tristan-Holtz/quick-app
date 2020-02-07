8181b38c9e058e1b005a52c1b1d74c96b3cc850e
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-02-07 15:48:01 -0700

    Populate animals

--- src/Animals.js
+++ src/Animals.js
@@ -1,19 +1,28 @@
-import React from 'react';
-
-const Animals = () => {
-  fetch('http://localhost:3001/api/v1/animals')
-    .then(response => response.json())
-    .then(data => animalElements(data));
-
-  const animalElements = animals => {
-    animals.map(animal => (
-      <article>
-        <h2>{animal.name}</h2>
-      </article>
-    ));
+import React, { Component } from 'react';
+class Animals extends Component {
+  state = {
+    animals: []
   };
 
-  return <section className="animals-section">{animalElements}</section>;
-};
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/animals')
+      .then(response => response.json())
+      .then(data => this.setState({ animals: data }));
+  }
 
+  render() {
+    console.log(this.state.animals);
+    return (
+      <section className="animals-section">
+        {this.state.animals.map(animal => (
+          <article>
+            <h2>{animal.name}</h2>
+            <p>They eat {animal.diet}</p>
+            <p>Fun Fact! {animal.fun_fact}</p>
+          </article>
+        ))}
+      </section>
+    );
+  }
+}
 export default Animals;
