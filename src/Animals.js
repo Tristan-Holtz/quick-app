0334d42e55c6ac7cf1d226d49eea8bb9a54eb5bd
Tristan Holtz <TristanHoltzConsulting@gmail.com>
2020-02-07 16:24:25 -0700

    Add delete functionality

--- src/Animals.js
+++ src/Animals.js
@@ -11,6 +11,24 @@ class Animals extends Component {
       .then(data => this.setState({ animals: data }));
   }
 
+  removeAnimal = e => {
+    console.log(e.target.value);
+    const toDelete = {
+      method: 'DELETE',
+      body: JSON.stringify({
+        id: e.target.value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    fetch(
+      `http://localhost:3001/api/v1/animals/${e.target.value}`,
+      toDelete
+    ).catch(error => console.log(error));
+  };
+
   render() {
     return (
       <section className="animals-section">
@@ -19,6 +37,9 @@ class Animals extends Component {
             <h2>{animal.name}</h2>
             <p>They eat {animal.diet}</p>
             <p>Fun Fact! {animal.fun_fact}</p>
+            <button value={animal.id} onClick={this.removeAnimal}>
+              Delete!?
+            </button>
           </article>
         ))}
       </section>
